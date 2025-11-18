5aab5c43cde9c421cf53b3904a834345f47c4d31
sinavarasina <varasinafarmadani@gmail.com>
2025-11-18 20:21:17 +0700

    fix animation

--- entities/player/anim.gd
+++ entities/player/anim.gd
@@ -2,49 +2,70 @@ extends Node
 class_name PlayerAnimation
 
 @onready var sprite: AnimatedSprite2D = get_parent().get_node("AnimatedSprite2D")
-var was_on_floor: bool = false
+
+enum State { IDLE, RUN, JUMP, PEAK, FALL, LAND }
+var state: State = State.IDLE
+
+const PEAK_THRESHOLD := 20.0
+const FALL_THRESHOLD := 60.0
+
+var was_on_ground := false
 
 func update_animation(dir: Vector2, vel: Vector2, body: CharacterBody2D) -> void:
 
-	if dir.x < 0.0:
+	var on_ground := body.is_on_floor()
+
+	if dir.x < 0:
 		sprite.flip_h = true
-	elif dir.x > 0.0:
+	elif dir.x > 0:
 		sprite.flip_h = false
 
-	var frames := sprite.sprite_frames
+	if not on_ground:
 
-	if not body.is_on_floor():
-		was_on_floor = false
-
-		if vel.y < 0.0:
-			if frames.has_animation("Jumping"):
-				if sprite.animation != "Jumping":
-					sprite.play("Jumping")
+		if vel.y < -PEAK_THRESHOLD:
+			_set_state(State.JUMP, "Jumping")
+			was_on_ground = false
 			return
 
-		elif vel.y > 0.0:
-			if frames.has_animation("Falling"):
-				if sprite.animation != "Falling":
-					sprite.play("Falling")
+		if abs(vel.y) <= PEAK_THRESHOLD:
+			_set_state(State.PEAK, "")
+			_hold_peak_frame()
+			was_on_ground = false
 			return
 
+		if vel.y > FALL_THRESHOLD:
+			_set_state(State.FALL, "Falling")
+			was_on_ground = false
+			return
+
+		return
+
+
+	if not was_on_ground:
+		_set_state(State.LAND, "Landing")
+		was_on_ground = true
+		return
+
+	if abs(dir.x) > 0.1:
+		_set_state(State.RUN, "Running")
 	else:
-		if not was_on_floor:
-			was_on_floor = true
-			if frames.has_animation("Landing"):
-				sprite.play("Landing")
-				return
+		_set_state(State.IDLE, "Idle")
 
-		if abs(dir.x) > 0.1:
-			if frames.has_animation("Running"):
-				if sprite.animation != "Running":
-					sprite.play("Running")
-			return
+func _set_state(new_state: State, anim_name: String) -> void:
+	if state == new_state:
+		return
 
-		if frames.has_animation("Idle"):
-			if sprite.animation != "Idle":
-				sprite.play("Idle")
-		else:
-			sprite.stop()
+	state = new_state
+	
+	if anim_name != null:
+		if sprite.sprite_frames.has_animation(anim_name):
+			sprite.stop()             
+			sprite.play(anim_name)      
 
-	was_on_floor = body.is_on_floor()
+func _hold_peak_frame():
+	if sprite.animation == "Jumping":
+		var count := sprite.sprite_frames.get_frame_count("Jumping")
+		if count > 0:
+			var mid := int(count / 2)
+			sprite.frame = mid
+			sprite.stop()   
