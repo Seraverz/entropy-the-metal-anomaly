9927ff5a51cd87ed54582be012817e1beb063553
glutt28 <michael.juntak@gmail.com>
2025-12-03 12:32:40 +0700

    Add new UI assets and scenes for game over and victory states
    - Imported new font: CyberpunkCraftpixPixel.otf for UI text.
    - Added general button texture and its import settings.
    - Included various UI frame textures and their import configurations.
    - Created game_over.tscn scene with buttons for Restart and Quit, and a Game Over label.
    - Developed victory.tscn scene featuring a victory message, a thank you button, and a banner.

--- scenes/ui/menus/MainMenu/main_menu.gd
+++ scenes/ui/menus/MainMenu/main_menu.gd
@@ -1,9 +1,9 @@
 extends Control
 
 func _ready() -> void:
-	$VBoxContainer/BtnStart.pressed.connect(_on_start_pressed)
-	$VBoxContainer/BtnSettings.pressed.connect(_on_settings_pressed)
-	$VBoxContainer/BtnQuit.pressed.connect(_on_quit_pressed)
+	$VBoxContainerStart/BtnStart.pressed.connect(_on_start_pressed)
+	$VBoxContainerSettings/BtnSettings.pressed.connect(_on_settings_pressed)
+	$VBoxContainerQuit/BtnQuit.pressed.connect(_on_quit_pressed)
 
 func _on_start_pressed() -> void:
 	SceneManager.load_level(SceneManager.LEVEL_1_PATH)
