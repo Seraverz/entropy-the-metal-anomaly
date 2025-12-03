c28a39b65adf65ff9f952686bee908b949758ff8
sinavarasina <varasinafarmadani@gmail.com>
2025-12-03 18:18:38 +0700

    link level 2

--- scenes/levels/level_2.gd
+++ scenes/levels/level_2.gd
@@ -1,10 +1,7 @@
-extends Node2D
-
 func _ready() -> void:
-	AudioManager.play_bgm("level_1")
+	#AudioManager.play_bgm("level_2")
 	GameState.level_completed.connect(_on_level_won)
 
 func _on_level_won() -> void:
 	GameState.level_completed.disconnect(_on_level_won)
-	#SceneManager.load_menu(SceneManager.VICTORY_PATH)
-	SceneManager.load_level(SceneManager.LEVEL_2_PATH)
+	SceneManager.load_menu(SceneManager.VICTORY_PATH)
